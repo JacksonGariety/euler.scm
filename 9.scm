f3f954375be269917618b601ba830b0de1b5fecb
Jackson Gariety <jackson@gariety.xxx>
2014-06-29 17:51:11 -0700

    Solution to #9.

--- 9.scm
+++ 9.scm
@@ -12,8 +12,30 @@
 ;; Find the product abc.
 
 ;; implementation
-(define (pythagorean-triplet n)
-  n)
+(define (pythagorean-triplet-product sum)
+  (define half-square (/ (expt sum 2) 2))
+  (let loop ((a (quotient sum 3)))
+    (define b (/ (- half-square (* sum a)) (- sum a)))
+    (if (inexact? b)
+        (loop (- a 1))
+        (let ((c (- sum b a)))
+          (* b a c)))))
 
 ;; execution
-(display (pythagorean-triplet 1000))
+(display (pythagorean-triplet-product 1000))
+
+;; I love this solution. It does so much with so
+;; little. The variable naming is great as well,
+;; (a), (b) and (c) corrospond with pythagorean
+;; theorum quite naturally (no pun intended).
+;;
+;; I did look up the math on this one. C solutions
+;; to this problem were quite helpful. I understand
+;; it, but that took a bit of reading about
+;; half-squares in computer science. That's the real
+;; magic here, and my goal is to be good enough at math
+;; to know why it's useful without Googling.
+;;
+;; The if-statement at the end is nice too. If a isn't
+;; an integer, we loop again incrementing a. If it is,
+;; we calculate c and return the product.
